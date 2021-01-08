1495e21b6bd28b71fd07b90798af759be4fd765a
Hokmah-ym <hokmahgrace@gamil.com>
2021-01-09 00:33:35 +0900

    FaceDetector 기능 구현

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { ActivityIndicator } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { ActivityIndicator, TouchableOpacity } from 'react-native';
 import { Camera } from 'expo-camera';
 import styled from 'styled-components/native';
+import { MaterialIcons } from '@expo/vector-icons';
+import * as FaceDetector from 'expo-face-detector';
+// import * as MediaLibrary from 'expo-media-library';
 
 const Container = styled.View`
   flex: 1;
@@ -17,6 +20,9 @@ const TitleText = styled.Text`
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.front);
+  const [smileDetected, setSmileDetected] = useState(false);
+  const cameraRef = useRef();
 
   useEffect(() => {
     (async () => {
@@ -41,12 +47,60 @@ export default function App() {
     );
   }
 
+  const switchCameraType = () => {
+    if (type === Camera.Constants.Type.front) {
+      setType(Camera.Constants.Type.back);
+    } else {
+      setType(Camera.Constants.Type.front);
+    }
+  };
+
+  const onFacesDetected = ({ faces }) => {
+    const face = faces[0];
+    if (face) {
+      if (face.smilingProbability > 0.7) {
+        setSmileDetected(true);
+      }
+      takePhoto();
+    }
+  };
+
+  const takePhoto = async () => {
+    try {
+      if (cameraRef) {
+        let { uri } = await cameraRef.current.takePictureAsync({ quality: 1, exif: true });
+        if (uri) {
+          savePhoto(uri);
+        }
+      }
+    } catch (e) {
+      alert(e);
+      setSmileDetected(false);
+    }
+  };
+
+  const savePhoto = async (uri) => {};
+
   return (
     <Container>
       <Camera
         style={{ width: '90%', height: '65%', borderRadius: 20, overflow: 'hidden' }}
-        type={Camera.Constants.Type.front}
+        type={type}
+        onFacesDetected={smileDetected ? null : onFacesDetected}
+        faceDetectorSettings={{
+          detectLandmarks: FaceDetector.Constants.Landmarks.all,
+          runClassifications: FaceDetector.Constants.Classifications.all,
+        }}
+        ref={cameraRef}
       ></Camera>
+      <TouchableOpacity onPress={switchCameraType}>
+        <MaterialIcons
+          name={type === Camera.Constants.Type.front ? 'camera-rear' : 'camera-front'}
+          style={{ marginTop: 40 }}
+          size={50}
+          color="#f1f3f5"
+        />
+      </TouchableOpacity>
     </Container>
   );
 }
